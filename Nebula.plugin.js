1375b9857a1f98dfbca8f70556b70c911002a378
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-09-14 16:42:39 -0700

    Update Nebula.plugin.js

--- Nebula.plugin.js
+++ Nebula.plugin.js
@@ -1,19 +1,20 @@
 /**
  * @name Nebula
  * @description Round, Deep sea blue, consistent, Nebula!
- * @version 1.2.2
+ * @version 1.0.0
  * @author Lorem#3481 & doggybootsy
+ * @source https://raw.githubusercontent.com/doggybootsy/Nebula/main/Nebula.plugin.js
  */
-const {Patcher: {after, unpatchAll}, alert, findModuleByProps, saveData, loadData, findModuleByDisplayName, Plugins:{isEnabled, get}, clearCSS, injectCSS, React: {createElement, Component}, showToast} = BdApi,
+const {Patcher, alert, findModuleByProps, saveData, loadData, findModuleByDisplayName, Plugins:{isEnabled, get}, clearCSS, injectCSS, React, showToast} = BdApi,
     SwitchItem = findModuleByDisplayName("SwitchItem"),
     request = require("request")
-class Switch extends Component {
+class Switch extends React.Component {
     constructor() {
         super(...arguments)
         this.state = { toggled: loadData("Nebula", "Button") ?? true }
     }
     render() {
-        return createElement(SwitchItem, {
+        return React.createElement(SwitchItem, {
             value: this.state.toggled,
             children: "Collpsable button",
             note: "Adds a button to collapse the left side of discord",
@@ -27,34 +28,36 @@ class Switch extends Component {
 }
 module.exports = class Nebula {
     patch() {
-        unpatchAll("nebula-inject")
+        Patcher.unpatchAll("nebula-inject")
         // Add button
         const HeaderBarContainer = findModuleByDisplayName("HeaderBarContainer")?.prototype,
             classes = findModuleByProps("iconWrapper","clickable"),
             tooltip = findModuleByProps("TooltipContainer")?.TooltipContainer,
             left = findModuleByProps("guilds", "container", "sidebar")
-        after("nebula-inject", HeaderBarContainer, "renderLoggedIn", (thisObject, _, res) => {
+        Patcher.after("nebula-inject", HeaderBarContainer, "renderLoggedIn", (thisObject, _, res) => {
             if (loadData("Nebula", "Button") === true) {
                 res?.props?.children.splice(0, 0, 
-                    createElement(tooltip, {
+                    React.createElement(tooltip, {
                         text: "Hide", 
                         position: "bottom",
                         children: [
-                            createElement("div", {
+                            React.createElement("div", {
                                 className: ["nebula-arrow",classes?.iconWrapper,classes?.iconWrapper].join(" "),
                                 onClick: () => {
-                                    if(isEnabled("Nebula") === true)for(const ite of [left?.guilds,left?.sidebar,"nebula-arrow"])document.getElementsByClassName(ite)[0]?.classList?.toggle("active")
-                                    else showToast("Nebula is disabled", {type:"warning", icon: true})
+                                    if(isEnabled("Nebula") === true && loadData("Nebula", "Button") === true)for(const ite of [left?.guilds,left?.sidebar,"nebula-arrow"])document.getElementsByClassName(ite)[0]?.classList?.toggle("active")
+                                    else {
+                                        document.querySelector(".nebula-arrow").remove()
+                                        loadData("Nebula", "Button") === false ? showToast("Collpsable button is disabled", {type:"warning", icon: true}) : showToast("Nebula is disabled", {type:"warning", icon: true})
+                                    }
                                 },
                                 children: [
-                                    createElement("svg", {
+                                    React.createElement("svg", {
                                         width: "24",
                                         height: "24",
                                         viewBox: "0 0 24 24",
-                                        fill: "none",
-                                        xmlns: "http://www.w3.org/2000/svg",
+                                        fill: "var(--interactive-normal)",
                                         children: [
-                                            createElement("path", {
+                                            React.createElement("path", {
                                                 d: "M15.535 3.515L7.05005 12L15.535 20.485L16.95 19.071L9.87805 12L16.95 4.929L15.535 3.515Z"
                                             })
                                         ]
@@ -68,20 +71,24 @@ module.exports = class Nebula {
         })
     }
     start() {
-        // If zlib use updater
-        if(global.ZeresPluginLibrary)
-            global.ZeresPluginLibrary.PluginUpdater.checkForUpdate("Nebula", get("Nebula").version, "https://raw.githubusercontent.com/doggybootsy/Nebula/main/Nebula.plugin.js")
+        document.body.classList.add("nebula")
         this.patch()
+        console.log("%c[Nebula has started]","background: #25294a; color: #d8dee9; font-size: 1.25em; padding: .2rem; border-radius: .35rem")
         // Fetch css | no githack
         request("https://raw.githubusercontent.com/Loremly/Nebula/main/index.css", (error, res, body) => {
-            if(error)alert("Nebula", "Couldn't fetch css")
-            else injectCSS("nebula-import", body)
+            if(error) alert("Nebula", "Couldn't fetch css")
+            else {
+                injectCSS("nebula-import", body)
+                showToast("Nebula was successfully imported", {type:"info", icon: true})
+            }
         })
     }
-    getSettingsPanel() {return createElement(Switch, {instance: this})}
+    getSettingsPanel() {return React.createElement(Switch, {instance: this})}
     stop() {
+        document.body.classList.remove("nebula")
         // Undo
-        unpatchAll("nebula-inject")
+        if(document.querySelector(".nebula-arrow"))document.querySelector(".nebula-arrow").remove()
+        Patcher.unpatchAll("nebula-inject")
         clearCSS("nebula-import")
         for(const ite of document.querySelectorAll(".active"))ite?.classList?.toggle("active")
     }
